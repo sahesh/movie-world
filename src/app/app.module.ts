46fabcc58578ac3fe8ff1dcaaf5d342ab5e03425
Sahesh RA <plusraizer@gmail.com>
2018-04-22 23:41:37 +0530

    Popular movies component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopularMoviesComponent
   ],
   imports: [
     BrowserModule
